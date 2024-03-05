085fe8fd98e4da8d1bae8d3e6c942df9d86ca930
Kacper <144652457+K4P1Z@users.noreply.github.com>
2024-03-05 09:31:48 +0100

    Add files via upload

--- klasa2ct/cos/dodaj_do_bazy_danych.php
+++ klasa2ct/cos/dodaj_do_bazy_danych.php
@@ -1,7 +1,14 @@
 <?php
+if(isset($_POST['imie']) && isset($_POST['nazwisko']) && isset($_POST['email'])){
 $imie = $_POST['imie'];
 $nazwisko = $_POST['nazwisko'];
 $email = $_POST['email'];
 
 echo "$imie, $nazwisko, $email";
+$doda_do_bazy = "INSERT INTO uczniowie(imie,nazwisko,email) values('$imie','$nazwisko','$email')";
+if($connect->exec($dodaj_do_bazy)) 
+}else{
+    echo "Co kukosz chopie...";
+    header("Refresh:3; url=0503.php");
+}
 ?>
